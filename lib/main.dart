6e9f3b5509af1d2ec97728ea8c0921dbc26a99a8
NANCY_LARA_BACA <a.22308051281225@cbtis128.edu.mx>
2025-03-04 17:09:43 +0000

    contenedores

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MiEjemplo extends StatelessWidget {
           title: Text('Ejemplo de contenedores'),
           centerTitle: true,
           titleTextStyle: const TextStyle(color: Colors.black, fontSize: 25),
-          backgroundColor: const Color(0xfff195b4),
+          backgroundColor: Colors.blue,
         ),
         body: Column(
           children: [
@@ -22,39 +22,40 @@ class MiEjemplo extends StatelessWidget {
               child: Container(
                 height: 50,
                 decoration: BoxDecoration(
-                  color: const Color(0xff680325), // Color de fondo
+                  color: Colors.red, // Color de fondo
                   borderRadius:
                       BorderRadius.circular(0), // Esquinas redondeadas
                 ),
                 child: const Center(
                   child: Text(
                     'Contenedor 1',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                    style: TextStyle(color: Color(0xff000000), fontSize: 18),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
             Container(
               width: 150,
               height: 200,
-              color: const Color(0xff9a5c70),
+              color: Colors.blue,
               child: Center(
                 child: Text(
                   'Contenedor 2',
-                  style: TextStyle(color: Color(0xff000000), fontSize: 18),
+                  style:
+                      const TextStyle(color: Color(0xff000000), fontSize: 18),
                 ),
               ),
             ),
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
             Container(
               width: double.infinity,
               height: 220,
-              color: Color(0xff4d122e),
+              color: Colors.amber,
               child: Center(
-                child: Text(
+                child: const Text(
                   'Contenedor 3',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+                  style: TextStyle(color: Color(0xff000000), fontSize: 18),
                 ),
               ),
             ),
